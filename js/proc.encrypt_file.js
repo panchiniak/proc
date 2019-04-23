a9010ec1302b1803fa52874cb8b9c198fd2d18ad
FERNANDES PANCHINIAK Rodrigo <panchiniakfernandesrodrigo@gmail.com>
2019-04-23 18:37:32 +0200

    WIP: fetch multiple pubkeys.

--- js/proc.encrypt_file.js
+++ js/proc.encrypt_file.js
@@ -74,7 +74,7 @@
                     // Still allowing a single recipient: [0] (the first taken)
                     var recipientPubkey = await Drupal.settings.proc.proc_recipient_pubkey;
 
-                    var recipientsPubkeys = await Drupal.settings.proc.proc_recipients_pubkeys;
+                    //var recipientsPubkeys = await Drupal.settings.proc.proc_recipients_pubkeys;
 
                     
 
@@ -83,34 +83,34 @@
                     'Version: OpenPGP.js v4.4.5',
                     'Comment: admin:empty@mail.localhost',
                     '',
-                    'xsBNBFy17BEBCADP7UPwGdngWgXkRFaFWzTOSK7xilrXUZYAy9FPIAnuJJfI',
-                    '49L6U65nlCAJQcPLKv6rKl+AAq+hdD4ipO9+nc8Y6oW7YHdhnpSMPtX83ynE',
-                    'W/Cs9kG/AeikOW7lrych3/4Zj9eFyXq7xrFCB44mFPNbac1w5VJygPKMIv4f',
-                    'co05jONMtHwDKrCdWnjGdYAX4QxFetPJ0UDc/knZYIh5YjXPerASYXoEL9wb',
-                    '2cgGzjyMrk4U90krFXHZ9aOQyjT2sL72QCg0GYdLYyiSrb8jSV+a4UqzCg1Q',
-                    'x2wEhAbkKnZfhNCUCN+uc3w01H5t7iyWwJa6A3SkzPVCZ4Q3E+d8dO3DABEB',
-                    'AAHNHGFkbWluIDxlbXB0eUBtYWlsLmxvY2FsaG9zdD7CwHUEEAEIAB8FAly1',
-                    '7BEGCwkHCAMCBBUICgIDFgIBAhkBAhsDAh4BAAoJEDmOsVi5nRL24jcIALx4',
-                    'PjsYU3y65k5uoHiZniHrkZ1JuXUM/FgsuQo+xOXL6bWyz+yEo3GOCz6CgqZd',
-                    'IwsrTVeutrwdHQIR6dCxeu9lmuN2g73AvHMbdqX3mja8FxNanwZdqveVpGTu',
-                    '4wZNeVFpGYBxGbr5xLMg1jNszUWCNZ/WKBEq9V2qc1aSp8VcvqsCmV09eOMa',
-                    'TaVHLxRSK6jDe4LJ11l7zF07snOwNM4pbJyf8m98flB7cvMSb35ZzSGlVv1O',
-                    'WXbsB2N8ffXWoN4YQBBx6FcZ3+0tep60Ahf+TqaD8AUFuf0XautdG1LbWhVu',
-                    'UsmEPVrOFPyqSVsDmHfkQsOT9Z73Sqaq2SDz4x7OwE0EXLXsEQEIAIDXnEoe',
-                    'Ahv1mwnYLYCk41cDVKvLaGCdgE/KQ5espBbfnYkxQRKrGek6nL3l11dB/kdZ',
-                    'MHxodtuiYBoPEsC80g7WDJrY2oeXB/1Nh/HPPslTnmZLo2fnMjQAgRaTVAtJ',
-                    's16H4KO5ypkUmeg7KSoIZ6M+WnpowXixhe4q+NRj9tHOjzcWLLjh7n/0O51E',
-                    'qsU36FXJyYarEuwUSUAHpZpP0hlmBS9rCdyNfvp7Or7syPT3aNElTff1rzSs',
-                    'FI7o3QeI0x0JtzizE6A1ztyuKTsjL2Z7Iv+RC76flPPeziHtAEpy47a/h9Tk',
-                    'Fv2EoTYu+0zV5kGDzSpjnvhU/HbW8JR9W3cAEQEAAcLAXwQYAQgACQUCXLXs',
-                    'EQIbDAAKCRA5jrFYuZ0S9qFSCACsYOpMxGOUCaSRdpf4LWGhgH4+eIX47Ivz',
-                    'M/o95lpUsZXFJQxp9zYceu/ogJLtpxpQiNng/JE+4VUZCuh2fTniR0bqDmzw',
-                    'lfmslal1PEOrO5UeMAVtsH+d/p+txgtUrwx6QzvCNrSl2ztMCGucqH1KzOOn',
-                    '1ci+JsyEYfsY0mXeFnduOnS33TyBJy2RUuh78wZK+NBi5Ey3UpAQ4txT1sgJ',
-                    'JEE+tPJrL0d+vnxr7AZj6bIXIru2Be/179u1ou33c126oCCGa9eNUD9mrZVv',
-                    'tTsnYh8L9AtxJYPspsd04mRlYiJzoRRpJ9F6IFjjTyIZ4Chj2e9ur0fWkrJ9',
-                    'HleyqVja',
-                    '=Tzy3',
+                    'xsBNBFy/KycBB/97FbTbwrrG5y+691cQ2u8jAmJSTGQKriBfmlbQ2pR2JpQd',
+                    'fO2R5RdWtlBF16syC7qiYu50HR5BX9WJZjrex4VBsx7/oOUG0/KYtmPnxWqn',
+                    '3elmdf+hoAorIu5goxhWwOmgG5H0kUxflIUGYPX01qTnpUCKLbKDmWAI8Y/Z',
+                    'QTAdn6HuIkyjIZrOATbQFneNiurJKI8FSfbLeAqShxVsRbKmdCc1ekVFIw3h',
+                    'm5fcIuzuQaMRFeVwiSSS99YoPGT5Q/anBiZSLydOyjeRGiCqqbon5t3dsHKq',
+                    'aGFQtpm3PSE9NwA3CndDJJtnzVq/ICFLTL4LaY/NGgTyraWjL+gUxTtnABEB',
+                    'AAHNHGFkbWluIDxlbXB0eUBtYWlsLmxvY2FsaG9zdD7CwHUEEAEIAB8FAly/',
+                    'KycGCwkHCAMCBBUICgIDFgIBAhkBAhsDAh4BAAoJEG+u4OTKgvhcLcIH/A4L',
+                    'bgD/wvhKpcpvwDsX7dlqgaMN6JMvjNJli0lJdjFOJNkdsPj9IYuX2N2GIsLn',
+                    'lMpOLSgEeSYnIiAz4nax4eJ6KdzEOTS/mW+1wySUkue691z06dPP2bpcUPYb',
+                    'ZCnPsM42tG0Mq+0gpElRp4tcFQ/oKSrK3Ik303mEReE21LMDIj1ZI+WEbmbL',
+                    'dsSXQ3xJ8Va6BqMlQncoOVCNgXtMdWzkhkOWWMDI3Dl+YQ1PS5QeAJssosO0',
+                    'mhCFW3MuOFZ60DYONEwTVStfdqGRn+oMjnOGYjN9IbaZJoT50NBVZ/x+yehr',
+                    'AOTMGU13CirY/AoeDWmfa/h1abfL1tl05NCy4qjOwE0EXL8rJwEIAJm12v4l',
+                    'wYsMk3DmZg+VLRK/n4wRmIAFMHe143DQIzeGcBX8JWwxti3z4Rhp0g6zx08x',
+                    '0xF0kEJ+CeY9DXDq8UV2m7rQIgvjZ8ysUMgy/n0o2nH3auJiC3AqzPHv1EWJ',
+                    'EQKoYPmKcKKPaR64l0MJvV54IQApliQGZ/+mV+FzP4li+KTODwhr1kSRUrgM',
+                    '5Y7G5ZMfCzGVkjtlwLRQXtzUi5iFRFhHmnPYh343WqErgqV1TFJQjYQtsGQP',
+                    'vOtZGAOZJqjw+r4grQUn++fbITL5srusixzXgZDE5DuopT8jz3k4Xt7f8ICD',
+                    '21iDE01VMBgnw6WWtj4hRmI3XmBlPAh7PIcAEQEAAcLAXwQYAQgACQUCXL8r',
+                    'JwIbDAAKCRBvruDkyoL4XHAXB/9Njk7vycZjXJ/Qu5tgLdCi/VCkTw624t+w',
+                    '9G/Z9uWJHzCe150uPSuV0rCSygz3Gt40fMrvpnPWrsLDVi7i2BlKCgH3JiIK',
+                    'EX+2SywlSNvQkv99lwsTnj+NlmAeuapKNQBQ7tz8YmNHNP/8n0qPfSXHR1Wj',
+                    'Rx9s+j+lYFfLIl2hFjJGVx+iXQV5p8d9L5I0WIdO5ATiLf1ytma3ZNiJGIji',
+                    'BwpLSXHJTx/ts6DIAfnSppmZNZlpesay9IHE2KGeCmWWp6y5U5/1LWwCkc9+',
+                    '/SZizAKO50EchgGztGS80UGM0f832mp9M5dld2bf9febsDPB4a1h3Xldp9C8',
+                    'bAd3p2OI',
+                    '=/zNT',
                     '-----END PGP PUBLIC KEY BLOCK-----'].join('\n');
 
 
@@ -150,6 +150,8 @@
                     
 
                     const keys = armoredPubkeys.map(pubkey => pubkey.armoredKey);
+                    
+                    console.log('----------------keys--------------');
                     console.log(keys);
 
 
@@ -157,15 +159,16 @@
                     // const testKeys = await recipientsPubkeys.map(async function (recipientsPubkeys) {
                     //   return await openpgp.key.readArmored(recipientsPubkeys).keys[0];
                     // });
-                    console.log(recipientsPubkeys);
-                    var i;
-                    var keyObjects = Array;
-                    for (i = 0; i < recipientsPubkeys.length; i++){
-                      keyObjects[i] = await openpgp.key.readArmored(recipientsPubkeys[i]).keys[0];
-                      //keyObjects[i] = recipientsPubkeys[i];
-                    }
+                    // console.log(recipientsPubkeys);
+                    // var i;
+                    // var keyObjects = Array;
+                    // for (i = 0; i < recipientsPubkeys.length; i++){
+                    //   keyObjects[i] = await openpgp.key.readArmored(recipientsPubkeys[i]).keys[0];
+                    //   //keyObjects[i] = recipientsPubkeys[i];
+                    // }
 
-                    console.log(keyObjects);
+                    // console.log('-------------keyObjects-------------------');
+                    // console.log(keyObjects);
 
 
 
