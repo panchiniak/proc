542fe481daa083c26e1605ddb007d0e08c2e708d
Rodrigo Panchiniak Fernandes <panchiniakfernandesrodrigo@gmail.com>
2020-03-19 22:12:11 +0100

    issue-proc-1: added get widget alter.

--- modules/proc_refield/proc_refield.api.php
+++ modules/proc_refield/proc_refield.api.php
@@ -13,3 +13,12 @@
  */
 function hook_proc_refield_widget_process_alter(array $element) {
 }
+
+/**
+ * Allow modules to alter proc refield widget list.
+ *
+ * @param array $proc_refield_widget
+ *   Form element of proc reference field.
+ */
+function hook_proc_refield_get_widget_alter(array $proc_refield_widget) {
+}
