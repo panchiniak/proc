6fd6e4a01691ab12e5849ae98d4b444284c1ec2d
Rodrigo Panchiniak Fernandes <panchiniakfernandesrodrigo@gmail.com>
2020-03-21 02:16:51 +0100

    issue-proc-1: added dependency.

--- modules/proc_refield/proc_refield.api.php
+++ modules/proc_refield/proc_refield.api.php
@@ -18,7 +18,18 @@ function hook_proc_refield_widget_process_alter(array $element) {
  * Allow modules to alter proc refield widget list.
  *
  * @param array $proc_refield_widget
- *   Form element of proc reference field.
+ *   List of available entity reference field widgets.
  */
 function hook_proc_refield_get_widget_alter(array $proc_refield_widget) {
+}
+
+/**
+ * Allow modules to alter add file link.
+ *
+ * @param string $add_file_link
+ *   Alterable array of cipher text data.
+ * @param array $context
+ *   Unalterable field instance and form element.
+ */
+function hook_proc_refield_add_file_link_alter(string $add_file_link, array $context) {
 }
