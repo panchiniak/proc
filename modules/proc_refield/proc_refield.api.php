91da7ca330eb6d22c7ef79859c1bb76838b5f69e
Rodrigo Panchiniak <panchiniakfernandesrodrigo@gmail.com>
2020-10-27 15:19:50 +0100

    Added delta for decryption link on multivalued field.

--- modules/proc_refield/proc_refield.api.php
+++ modules/proc_refield/proc_refield.api.php
@@ -29,7 +29,18 @@ function hook_proc_refield_get_widget_alter(array $proc_refield_widget) {
  * @param string $add_file_link
  *   Alterable array of cipher text data.
  * @param array $context
- *   Unalterable field instance and form element.
+ *   Unalterable context enriched by field instance and form element.
  */
 function hook_add_proc_file_link_alter(string $add_file_link, array $context) {
+}
+
+/**
+ * Allow modules to alter decrypt file link.
+ *
+ * @param string $decrypt_file_link
+ *   Alterable string of decryption link.
+ * @param array $context
+ *   Unalterable field widget context enriched by field instance and form element objects.
+ */
+function hook_decrypt_proc_file_link_alter(string $decrypt_file_link, array $context) {
 }
