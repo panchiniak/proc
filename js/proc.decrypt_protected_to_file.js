40273ae8e477c54a254f653c8f8daff0db1fe0ec
Rodrigo Panchiniak <panchiniakfernandesrodrigo@gmail.com>
2021-03-17 23:41:36 +0100

    Added decryption of multiple cached files.

--- js/proc.decrypt_protected_to_file.js
+++ js/proc.decrypt_protected_to_file.js
@@ -7,19 +7,25 @@
     Drupal.behaviors.proc = {
         attach: function (context, settings) {
 
-            let procJsLabels   = Drupal.settings.proc.proc_labels,
-                passDrupal     = Drupal.settings.proc.proc_pass,
-                privkey        = Drupal.settings.proc.proc_privkey,
-                cipherId       = Drupal.settings.proc.proc_id,
-                cipherChanged  = Drupal.settings.proc.proc_changed,
-                sourceFileName = Drupal.settings.proc.proc_source_file_name,
-                sourceFileSize = Drupal.settings.proc.proc_source_file_size,
-                fileApiErrMsg  = Drupal.settings.proc.proc_fileapi_err_msg;
+            let procJsLabels      = Drupal.settings.proc.proc_labels,
+                passDrupal        = Drupal.settings.proc.proc_pass,
+                privkey           = Drupal.settings.proc.proc_privkey,
+                cipherIds         = Drupal.settings.proc.proc_ids,
+                ciphersChanged    = Drupal.settings.proc.procs_changed,
+                sourcesFileNames  = Drupal.settings.proc.proc_sources_file_names,
+                sourcesFilesSizes = Drupal.settings.proc.proc_sources_file_sizes,
+                fileApiErrMsg     = Drupal.settings.proc.proc_fileapi_err_msg;
 
-            let procURL = `${window.location.origin + Drupal.settings.basePath}proc/api/get/${cipherId}/?cipherchanged=${cipherChanged}`;
-            let cachedCipher;
-            let cipherResponse;
-            let cipher;
+            let procURLs = [];
+
+            cipherIds.forEach(
+                function (cipherId,cipherIdIndex){
+                    procURLs[cipherIdIndex] = `${window.location.origin + Drupal.settings.basePath}proc/api/get/${cipherIds[cipherIdIndex]}/?cipherchanged=${ciphersChanged[cipherIdIndex]}`;
+                }
+            );
+
+            let cipherResponse,
+                cipher;
 
             const introducingKeyDecryptionMsgElement = `<div class="messages info proc-info" id="proc-decrypting-info">${procJsLabels.proc_introducing_decryption}</div>`;
 
@@ -48,117 +54,129 @@
                 }
             );
 
-
             if ('caches' in self){
                 let cache = caches.open('proc');
-                cachedCipher = cache.then(async function (cache){
-                    let response = await cache.match(procURL);
-                    if (response){
-                        console.info('Reusing cipher from cache');
-                        return;
-                    }
-                    else{
-                        console.info('Adding cipher to cache');
-                        cipherResponse = (await fetch(procURL));
-                        cipher = cipherResponse.clone();
-                        cache.put(procURL, cipherResponse);
-                        return cipher;
-                        // @TODO: add clean up of expired cache if any!
-                    }
-                });
-                caches.match(procURL).then(async function(result){
-                    if (result){
-                        cipher = result.json().then(async function(result){
-                            return result.cipher;
+
+                cipherIds.forEach(
+                    function (cipherId,cipherIdIndex){
+                        let cachedCiphers = [];
+
+                        cachedCiphers[cipherIdIndex] = cache.then(async function (cache){
+                            let response = await cache.match(procURLs[cipherIdIndex]);
+                            if (response){
+                                console.info('Reusing cipher from cache');
+                                return;
+                            }
+                            else{
+                                console.info('Adding cipher to cache');
+                                cipherResponse = (await fetch(procURLs[cipherIdIndex]));
+                                cipher = cipherResponse.clone();
+                                cache.put(procURLs[cipherIdIndex], cipherResponse);
+                                return cipher;
+                                // @TODO: add clean up of expired cache if any!
+                            }
                         });
                     }
-                    else{
-                        cipher = (await cachedCipher);
-                        cipher = cipher.json().then(async function(result){
-                            return result.cipher;
-                        });
-                    }
-                    cipher.then(async function(cipherText){
-                        $('#decryption-link').on(
-                            'click', async function () {
-                                let secretPass       = $('input[name=pass]')[0].value,
-                                    secretPassString = secretPass,
-                                    passphrase       = passDrupal.concat(secretPassString);
+                );
 
-                                const privKeyObj = (await openpgp.key.readArmored(privkey)).keys[0];
+                $('#decryption-link').on(
+                    'click', async function (e) {
+                        e.preventDefault();
 
-                                await privKeyObj.decrypt(passphrase).catch(
-                                    function (err) {
-                                        $('form#-proc-decrypt-to-file').prepend(`<div class="messages error">${Drupal.t(err)}</div>`);
-                                        if ($('a#decryption-link')[0].href) {
-                                            const fileUrl = $('a#decryption-link')[0].href;
-                                            URL.revokeObjectURL(fileUrl);
-                                            $('a#decryption-link').removeAttr('href');
-                                        }
-                                    }
-                                );
+                        let secretPass       = $('input[name=pass]')[0].value,
+                            secretPassString = secretPass,
+                            passphrase       = passDrupal.concat(secretPassString);
 
-                                if (!$('#proc-decrypting-info')[0]) {
-                                    $('form#-proc-decrypt-to-file').prepend(introducingKeyDecryptionMsgElement);
-                                }
+                        const privKeyObj = (await openpgp.key.readArmored(privkey)).keys[0];
 
-                                const
-                                    optionsDecription = {
-                                        message: await openpgp.message.readArmored(cipherText).catch(
-                                            function (err) {
-                                                let messageError = `<div class="messages error">${Drupal.t(err)}</div>`;
-                                                $('form#-proc-decrypt-to-file').prepend(messageError);
-                                            }
-                                        ),
-                                        privateKeys: [privKeyObj],
-                                        // For the sake of simplicity all files are considered binary.
-                                        format: 'binary'
-                                    },
-                                    decrypted         = await openpgp.decrypt(optionsDecription).catch(
-                                        function (err) {
-                                            let messageError = `<div class="messages error">${Drupal.t(err)}</div>`;
-                                            $('form#-proc-decrypt-to-file').prepend(messageError);
-                                            $(":focus").blur();
-                                            if ($('.messages.info.proc-info:first')){
-                                                $('.messages.info.proc-info:first').remove();
-                                            }
-                                        }
-                                    );
-
-                                if (decrypted){
-                                    const
-                                        plaintext = await openpgp.stream.readToEnd(decrypted.data),
-                                        blob = new Blob(
-                                            [plaintext], {
-                                                type: 'application/octet-binary',
-                                                endings: 'native'
-                                            }
-                                        ),
-                                        link = $('#decryption-link');
-
-                                    link.attr('href', URL.createObjectURL(blob));
-                                    let openActionLabel = procJsLabels.proc_open_file_state;
-                                    if (link.text() != openActionLabel) {
-                                        link.text(openActionLabel);
-                                        // Highlight the link for better UX
-                                        link.removeClass('active');
-                                        $('.messages').after(`<div class="messages status" id="proc-decrypting-status">${procJsLabels.proc_decryption_success}</div>`);
-                                    }
-
-                                    // Check if file generated is the same size of source file.
-                                    if (blob.size.toString() === sourceFileSize) {
-                                        // Restore original file name:
-                                        link.attr('download', sourceFileName);
-                                    } else {
-                                        // @TODO: save error log.
-                                        $('form#-proc-decrypt-to-file').prepend(`<div class="messages error">${procJsLabels.proc_decryption_size_mismatch}</div>`);
-                                    }
+                        await privKeyObj.decrypt(passphrase).catch(
+                            function (err) {
+                                $('form#-proc-decrypt-to-file').prepend(`<div class="messages error">${Drupal.t(err)}</div>`);
+                                if ($('a#decryption-link')[0].href) {
+                                    const fileUrl = $('a#decryption-link')[0].href;
+                                    URL.revokeObjectURL(fileUrl);
+                                    $('a#decryption-link').removeAttr('href');
                                 }
                             }
                         );
 
-                    });
-                });
+                        if (!$('#proc-decrypting-info')[0]) {
+                            $('form#-proc-decrypt-to-file').prepend(introducingKeyDecryptionMsgElement);
+                        }
+
+                        let temporaryDownloadLink = document.createElement("a");
+                        temporaryDownloadLink.style.display = 'none';
+                        document.body.appendChild(temporaryDownloadLink);
+
+                        cipherIds.forEach(
+                            async function (cipherId, cipherIndex){
+                                let cache = caches.match(procURLs[cipherIndex]);
+                                cache.then(
+                                    async function (response){
+                                        response.json().then(async function (data){
+                                            let cipherText = data.ciphers[0];
+
+                                            const
+                                                optionsDecription = {
+                                                    message: await openpgp.message.readArmored(cipherText).catch(
+                                                        function (err) {
+                                                            let messageError = `<div class="messages error">${Drupal.t(err)}</div>`;
+                                                            $('form#-proc-decrypt-to-file').prepend(messageError);
+                                                        }
+                                                    ),
+                                                    privateKeys: [privKeyObj],
+                                                    // For the sake of simplicity all files are considered binary.
+                                                    format: 'binary'
+                                                },
+                                                decrypted = await openpgp.decrypt(optionsDecription).catch(
+                                                    function (err) {
+                                                        let messageError = `<div class="messages error">${Drupal.t(err)}</div>`;
+                                                        $('form#-proc-decrypt-to-file').prepend(messageError);
+                                                        $(":focus").blur();
+                                                        if ($('.messages.info.proc-info:first')){
+                                                            $('.messages.info.proc-info:first').remove();
+                                                        }
+                                                    }
+                                                );
+                                                if (decrypted){
+                                                    const
+                                                        plaintext = await openpgp.stream.readToEnd(decrypted.data),
+                                                        blob = new Blob(
+                                                            [plaintext], {
+                                                                type: 'application/octet-binary',
+                                                                endings: 'native'
+                                                            }
+                                                        ),
+                                                        link = $('#decryption-link');
+
+                                                    temporaryDownloadLink.setAttribute( 'href', URL.createObjectURL(blob));
+
+                                                    let openActionLabel = procJsLabels.proc_open_file_state;
+                                                    if (link.text() != openActionLabel) {
+                                                        link.text(openActionLabel);
+                                                        // Highlight the link for better UX
+                                                        link.removeClass('active');
+                                                        $('.messages').after(`<div class="messages status" id="proc-decrypting-status">${procJsLabels.proc_decryption_success}</div>`);
+                                                    }
+
+                                                    // Check if file generated is the same size of source file.
+                                                    if (blob.size.toString() === sourcesFilesSizes[cipherIndex]) {
+                                                        // Restore original file name:
+                                                        temporaryDownloadLink.setAttribute( 'download', sourcesFileNames[cipherIndex]);
+                                                        temporaryDownloadLink.click();
+                                                    } else {
+                                                        // @TODO: save error log.
+                                                        $('form#-proc-decrypt-to-file').prepend(`<div class="messages error">${procJsLabels.proc_decryption_size_mismatch}</div>`);
+                                                    }
+                                                }
+                                        });
+                                    }
+                                );
+                            }
+                        );
+                        document.body.removeChild( temporaryDownloadLink );
+                    }
+                );
             }
             else{
                 alert('Error: this browser does not suport cache API. Please use an updated browser.');
