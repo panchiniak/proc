4d78a0b1483e9bbb9c20f519299081e4d5356e9a
Rodrigo Panchiniak Fernandes <panchiniakfernandesrodrigo@gmail.com>
2023-03-08 14:15:02 +0000

    Added settings fields.

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -28,19 +28,29 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['example'] = [
+    $form['proc-stream-wrapper'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Example'),
-      '#default_value' => $this->config('proc.settings')->get('example'),
+      '#title' => $this->t('Global stream wrapper'),
+      '#description' => $this->t('Set a stream wrapper for the storage of cipher texts.'),
+      '#default_value' => $this->config('proc.settings')->get('proc-stream-wrapper'),
     ];
-    $classes = get_declared_classes();
-    ksm($classes);
-    
-    // var_dump($classes);
-    // foreach ($classes as $class) {
-    //   var_dump($class . '<br>');
-    // }
-      
+    $form['proc-enable-stream-wrapper'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable stream wrapper globaly'),
+      '#description' => $this->t('Enable stream wrapper storage of cipher texts.'),
+      '#default_value' => $this->config('proc.settings')->get('proc-enable-stream-wrapper'),
+    ];
+    $form['proc-rsa-key-size'] = [
+      '#type' => 'select',
+      '#title' => $this->t('RSA keys size'),
+      '#options' => [
+        '2048' => $this->t('2048'),
+        '4096' => $this->t('4096'),
+      ],
+      '#empty_option' => $this->t('-select-'),
+      '#description' => $this->t('Set the RSA key size.'),
+      '#default_value' => $this->config('proc.settings')->get('proc-rsa-key-size'),
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -48,10 +58,10 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if ($form_state->getValue('example') != 'example') {
-      $form_state->setErrorByName('example', $this->t('The value is not correct.'));
-    }
-    parent::validateForm($form, $form_state);
+    // if ($form_state->getValue('example') != 'example') {
+    //   $form_state->setErrorByName('example', $this->t('The value is not correct.'));
+    // }
+    // parent::validateForm($form, $form_state);
   }
 
   /**
@@ -59,9 +69,10 @@ class SettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('proc.settings')
-      ->set('example', $form_state->getValue('example'))
+      ->set('proc-stream-wrapper', $form_state->getValue('proc-stream-wrapper'))
+      ->set('proc-enable-stream-wrapper', $form_state->getValue('proc-enable-stream-wrapper'))
+      ->set('proc-rsa-key-size', $form_state->getValue('proc-rsa-key-size'))
       ->save();
     parent::submitForm($form, $form_state);
   }
-
 }
