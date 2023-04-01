b862cbe540c0211c0bfe9f911f3e4a7b4941c119
Rodrigo Panchiniak Fernandes <panchiniakfernandesrodrigo@gmail.com>
2023-04-01 12:40:32 +0000

    Added widget settings.

--- src/Plugin/Field/FieldType/ProcEntityReferenceFieldItem.php
+++ src/Plugin/Field/FieldType/ProcEntityReferenceFieldItem.php
@@ -10,8 +10,23 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\TypedData\DataDefinition;
 use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
 use Drupal\proc\Plugin\Field\FieldWidget;
-// use Drupal\Core\Field\FieldType\EntityReferenceItem;
-
+use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Entity\ContentEntityStorageInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Entity\TypedData\EntityDataDefinition;
+use Drupal\Core\Field\FieldException;
+use Drupal\Core\Field\PreconfiguredFieldUiOptionsInterface;
+use Drupal\Core\Form\OptGroup;
+use Drupal\Core\Render\Element;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TypedData\DataReferenceDefinition;
+use Drupal\Core\TypedData\DataReferenceTargetDefinition;
+use Drupal\Core\TypedData\OptionsProviderInterface;
+use Drupal\Core\Validation\Plugin\Validation\Constraint\AllowedValuesConstraint;
 
 /**
  * Defines the 'proc_proc_entity_reference_field' field type.
@@ -25,161 +40,74 @@ use Drupal\proc\Plugin\Field\FieldWidget;
  *   default_formatter = "entity_reference_label",
  *   list_class = "\Drupal\Core\Field\EntityReferenceFieldItemList",
  * )
- * 
- * @DCG
- * If you are implementing a single value field type you may want to inherit
- * this class form some of the field type classes provided by Drupal core.
- * Check out /core/lib/Drupal/Core/Field/Plugin/Field/FieldType directory for a
- * list of available field type implementations.
  */
 class ProcEntityReferenceFieldItem extends EntityReferenceItem {
-
-// * @FieldType(
-// *   id = "proc_proc_entity_reference_field",
-// *   label = @Translation("Proc Entity Reference Field"),
-// *   description = @Translation("An entity field containing an proc enabled entity reference."),
-// *   category = @Translation("Reference"),
-// *   default_widget = "proc_proc_entity_reference_field_widget",
-// *   default_formatter = "entity_reference_label",
-// *   list_class = "\Drupal\Core\Field\EntityReferenceFieldItemList",
-// * )
-
-
-
+  /**
+  * {@inheritdoc}
+  */
+  public static function defaultFieldSettings() {
+    $settings = ['target_type' => 'proc'];
+    return $settings + parent::defaultFieldSettings();
+  }
 
   /**
    * {@inheritdoc}
    */
-  // public static function defaultFieldSettings() {
-  //   $settings = parent::defaultFieldSettings();
-  //   // Add any custom field settings here.
-  //   return $settings;
-  // }
+  public function fieldSettingsForm(array $form, FormStateInterface $form_state) {
+    
+    // $form = parent::fieldSettingsForm($form, $form_state);
+    
+    
+    $form['proc'] = [
+      '#type' => 'details',
+      '#title' => t('Protected Content Settings for this field'),
+      '#open' => TRUE,
+    ];
+    
+    $form['proc']['proc_field_recipients_fetcher_endpoint'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Endpoint for fetching user IDs of recipients'),
+      '#description' => $this->t('Leave it empty for direct fetcher'),
+      '#default_value' => $this->getSetting('proc_field_recipients_fetcher_endpoint'),
+    ];
 
-  // /**
-  // * {@inheritdoc}
-  // */
-  // public function schema() {
-  //   $schema = parent::schema();
-  //   // Add any custom schema definitions here.
-  //   return $schema;
-  // }
+    $form['proc']['proc_field_recipients_manual_fetcher'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Define the user IDs of recipients in a CSV list'),
+      '#default_value' => $this->getSetting('proc_field_recipients_manual_fetcher'),
+    ];
 
-  // /**
-  // * {@inheritdoc}
-  // */
-  // public function fieldSettingsForm(array $form, FormStateInterface $form_state) {
-  //   $element = parent::fieldSettingsForm($form, $form_state);
-  //   // Add any custom field settings form elements here.
-  //   return $element;
-  // }
+    $form['proc']['proc_field_mode'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Modes of operation'),
+      '#default_value' => $this->getSetting('proc_field_mode'),
+      '#options' => [
+        0 => $this->t('Disabled'),
+        1 => $this->t('Only encryption'),
+        2 => $this->t('Encryption and signature'),
+      ],
+    ];
 
-  
-  // Annotation suggested:
-  // * @FieldWidget(
-  // *   id = "my_entity_reference",
-  // *   label = @Translation("My Entity Reference"),
-  // *   description = @Translation("An entity reference field that refers to my custom entity."),
-  // *   field_types = {
-  // *     "entity_reference"
-  // *   }
-  // * )  
+    $form['proc']['proc_field_input_mode'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Modes of input'),
+      '#default_value' => $this->getSetting('proc_field_input_mode'),
+      '#options' => [
+        0 => $this->t('File'),
+        1 => $this->t('Text area'),
+        2 => $this->t('Text field'),
+      ],
+    ];
+    
+    $form['bar'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Bar'),
+      '#default_value' => $this->getSetting('bar'),
+    ];
 
-  // /**
-  // * {@inheritdoc}
-  // */
-  // public static function defaultFieldSettings() {
-  //   $settings = ['bar' => 'beer'];
-  //   return $settings + parent::defaultFieldSettings();
-  // }
+    $form += parent::fieldSettingsForm($form, $form_state);
 
-  // /**
-  // * {@inheritdoc}
-  // */
-  // public function fieldSettingsForm(array $form, FormStateInterface $form_state) {
-
-  //   $element['bar'] = [
-  //     '#type' => 'textfield',
-  //     '#title' => $this->t('Bar'),
-  //     '#default_value' => $this->getSetting('bar'),
-  //   ];
-
-  //   return $element;
-  // }
-
-  // /**
-  // * {@inheritdoc}
-  // */
-  // public function isEmpty() {
-  //   $value = $this->get('value')->getValue();
-  //   return $value === NULL || $value === '';
-  // }
-
-  // /**
-  // * {@inheritdoc}
-  // */
-  // public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
-
-  //   // @DCG
-  //   // See /core/lib/Drupal/Core/TypedData/Plugin/DataType directory for
-  //   // available data types.
-  //   $properties['value'] = DataDefinition::create('string')
-  //     ->setLabel(t('Text value'))
-  //     ->setRequired(TRUE);
-
-  //   return $properties;
-  // }
-
-  // /**
-  // * {@inheritdoc}
-  // */
-  // public function getConstraints() {
-  //   $constraints = parent::getConstraints();
-
-  //   $constraint_manager = \Drupal::typedDataManager()->getValidationConstraintManager();
-
-  //   // @DCG Suppose our value must not be longer than 10 characters.
-  //   $options['value']['Length']['max'] = 10;
-
-  //   // @DCG
-  //   // See /core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint
-  //   // directory for available constraints.
-  //   $constraints[] = $constraint_manager->create('ComplexData', $options);
-  //   return $constraints;
-  // }
-
-  // /**
-  // * {@inheritdoc}
-  // */
-  // public static function schema(FieldStorageDefinitionInterface $field_definition) {
-
-  //   $columns = [
-  //     'value' => [
-  //       'type' => 'varchar',
-  //       'not null' => FALSE,
-  //       'description' => 'Column description.',
-  //       'length' => 255,
-  //     ],
-  //   ];
-
-  //   $schema = [
-  //     'columns' => $columns,
-  //     // @DCG Add indexes here if necessary.
-  //   ];
-
-  //   return $schema;
-  // }
-
-  // /**
-  // * {@inheritdoc}
-  // */
-  // public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
-  //   $random = new Random();
-  //   $values['value'] = $random->word(mt_rand(1, 50));
-  //   return $values;
-  // }
-  
-  // Implement any additional methods that your custom field type needs, such as viewElements(), 
-  // formElement(), and propertyDefinitions().  
+    return $form;
+  }
 
 }
